4aeed8dd8a633f0312fcf9c4f96e8da55e085b14
TilteD24 <21bcs080@iiitdmj.ac.in>
2024-08-26 15:45:29 +0530

    added spinner

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 import "./index.css";
 
 function Home({ problems }) {
+  let [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (problems && problems.length > 0) {
+      setLoading(false);
+    }
+  }, [problems]);
+
   return (
     <>
       <h3>Problems</h3>
-      {problems &&
+      <hr />
+      <br />
+      {loading ? (
+        <div
+          className="spinner-container"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "10vh",
+          }}
+        >
+          <ClipLoader color="gray" size={100} />
+        </div>
+      ) : (
         problems.map((problem) => (
           <div className="problem-container">
             <h2 className="problem-id">{problem.pid}</h2>
@@ -13,7 +37,8 @@ function Home({ problems }) {
               <h2 className="problem-title">{problem.title}</h2>
             </Link>
           </div>
-        ))}
+        ))
+      )}
     </>
   );
 }
